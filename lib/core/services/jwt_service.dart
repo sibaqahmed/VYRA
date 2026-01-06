381c8c9698a1fbad177346d6c90df4ca1b18ea85
Sibaq Ahmed <sibaqahmed@gmail.com>
2026-01-06 15:53:32 +0530

    Second Final commit for vyra

--- lib/core/services/jwt_service.dart
+++ lib/core/services/jwt_service.dart
@@ -2,27 +2,27 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class JwtService {
+  static const String _baseUrl =
+      "https://vyra-backend-si7x.onrender.com";
+
   static Future<String> fetchJwt({
     required String room,
     required String name,
     required bool moderator,
   }) async {
     final uri = Uri.parse(
-      "http://10.0.2.2:3000/jwt"
-          "?room=$room"
-          "&name=$name"
+      "$_baseUrl/jwt"
+          "?name=$name"
           "&moderator=$moderator",
     );
 
     final response = await http.get(uri);
 
     if (response.statusCode != 200) {
-      throw Exception("Failed to fetch JWT");
+      throw Exception("Failed to fetch JWT: ${response.body}");
     }
 
     final data = jsonDecode(response.body);
-
-    // 🔥 THIS IS THE FIX
-    return data["token"]; // ✅ return ONLY the JWT string
+    return data["token"];
   }
 }
